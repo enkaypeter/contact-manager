1936b8b59e2f7987e2f6ea9f71cf59f942ea4370
enkay <ogbulu@mercurie.ng>
2019-05-16 17:29:14 +0100

    Updates

---
+++ precache-manifest.f04c9768942515c838c24c41efee7848.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cea69f3ed8aec96d98cf",
+    "url": "/contact-manager/static/css/main.1fe0d43d.chunk.css"
+  },
+  {
+    "revision": "cea69f3ed8aec96d98cf",
+    "url": "/contact-manager/static/js/main.cea69f3e.chunk.js"
+  },
+  {
+    "revision": "a25dc548b2853493d42e",
+    "url": "/contact-manager/static/js/runtime~main.a25dc548.js"
+  },
+  {
+    "revision": "78a8a94c6738c3c6ee2c",
+    "url": "/contact-manager/static/css/2.f426739a.chunk.css"
+  },
+  {
+    "revision": "78a8a94c6738c3c6ee2c",
+    "url": "/contact-manager/static/js/2.78a8a94c.chunk.js"
+  },
+  {
+    "revision": "288c64c3722acca3a1f01d8ecfe6867f",
+    "url": "/contact-manager/index.html"
+  }
+];
